4d1d8f2c1323b276fe14be795465350fb72a9f16
BIecoSRL <rodriguez_douglas@hotmail.com>
2025-07-18 23:52:24 -0400

    Update CustomerView.jsx

--- src/components/views/CustomerView.jsx
+++ src/components/views/CustomerView.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { supabase } from '../../../lib/supabaseClient';
+import { supabase } from '../../lib/supabaseClient';
 
 export default function CustomerView() {
   const { slug } = useParams();
